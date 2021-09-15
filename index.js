c96c5d8e61c7cce0fa6e26431e0b58f45a884fd8
Jackson <53550653+its-jackson@users.noreply.github.com>
2021-09-14 21:39:58 -0400

    Added projects page

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ app.use(express.static(__dirname+'/public'));
 // use res.render to load up an ejs view file
 // index page
 app.get('/', function(req, res) {
+
   var projects = [
     { name: 'Polymorphic Auto Woodcutter', description: "advanced AIO woodcutting script", released: 2021},
     { name: 'Motherlode Mine X', description: "advanced motherlode mine script", released: 2021},
     { name: 'Sulliuscep X', description: "advanced sulliuscep script", released: 2021}
   ];
 
-  var tagline = "'No programming concept is complete without polymorphism'";
+  var tagline = "'No programming concept is complete without polymorphism.'";
 
   res.render('pages/index', {
     projects: projects,
@@ -27,7 +28,28 @@ app.get('/', function(req, res) {
 
 // about page
 app.get('/about', function(req, res) {
-  res.render('pages/about');
+
+  var tagline = "Also known as Polymoprhic - TRiBot";
+
+  res.render('pages/about', {
+    tagline: tagline
+  });
+});
+
+// projects page
+app.get('/projects', function(req, res) {
+
+  var tagline = "'If you only knew the magnificence of the 3.'";
+  var woodcutter = "Polymorphic Auto Woodcutter";
+  var motherlode = "Motherlode Mine X";
+  var sulliuscep = "Sulliuscep X";
+
+  res.render('pages/projects', {
+    tagline: tagline,
+    woodcutter: woodcutter,
+    motherlode: motherlode,
+    sulliuscep: sulliuscep
+  });
 });
 
 // port for server 8080
